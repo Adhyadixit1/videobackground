0e7f159bca7b9344efcfce9dc3600fe11cd19a80
Santhosh VS <santhosh02vs@gmail.com>
2025-09-07 17:29:49 +0530

    content: update hero, agency, and bottom sections with new text and media

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -72,11 +72,11 @@ const Agence = () => {
                 </div>
                 <div className='relative font-[font2]'>
                     <div className='lg:mt-[55vh] mt-[30vh]'>
-                        <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
-                            Douze</h1>
+                        <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>SEVEN7Y <br />
+                            TWO</h1>
                     </div>
                     <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
-                        <p className='lg:text-6xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+                        <p className='lg:text-6xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
                     </div>
                 </div>
             </div>
